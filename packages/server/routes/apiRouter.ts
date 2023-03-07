2e0a2018b4a403916f488d3a7168a92d9d69669a
Varun Nadgir <theminivann@gmail.com>
2023-03-07 17:44:48 -0500

    cleanup router

--- packages/server/routes/apiRouter.ts
+++ packages/server/routes/apiRouter.ts
@@ -2,8 +2,4 @@ import express from "express";
 
 const apiRouter = express.Router();
 
-apiRouter.get("/test", (req, res) => {
-  res.status(200).send({ message: "HELL YEAH BROTHER! THAT THERE API IS DONE THERE CERRRRNECTED!" });
-});
-
 export default apiRouter;
